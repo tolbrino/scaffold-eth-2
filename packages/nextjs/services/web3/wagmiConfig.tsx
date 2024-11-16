b3f5164013c74e3d9b347c1bd1b97043476a465d
Tino Breddin <t@tolbrino.com>
2024-11-16 17:00:13 +0700

    Working uhttp integration

--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -7,7 +7,7 @@ import scaffoldConfig from "~~/scaffold.config";
 import { getAlchemyHttpUrl } from "~~/utils/scaffold-eth";
 
 const { targetNetworks, usePrivateRpc, pollingInterval, uhttpClientId } = scaffoldConfig;
-const router = new Routing.Client(uhttpClientId, { forceZeroHop: true, clientAssociatedExitNodes: true });
+const router = new Routing.Client(uhttpClientId, { forceZeroHop: true, clientAssociatedExitNodes: false });
 
 // We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
 export const enabledChains = targetNetworks.find((network: Chain) => network.id === 1)
@@ -27,32 +27,34 @@ export const wagmiConfig = createConfig({
     // The plain transport is used when usePrivateRpc is false
     const fallbackTransport = fallback(rpcFallbacks);
 
+    // If alchemy is not available, we need to manually fetch the provider
+    // from viem/chains
     const provider = alchemyHttpUrl || chain?.rpcUrls.default.http[0];
-
-    console.log(`CHAIN: ${chain.id}`);
-    console.log(`ALCHEMY HTTP URL: ${alchemyHttpUrl}`);
-    console.log(`USE PRIVATE RPC: ${usePrivateRpc}`);
-    console.log(`UHTTP CLIENT ID: ${uhttpClientId}`);
-    console.log(`CLIENT PROVIDER: ${provider}`);
-    console.log(`isHardhat: ${isHardhat}`);
 
     // The custom private transport is used when usePrivateRpc is true
     const customTransport = custom({
-      async request(req) {
+      async request({ method, params }) {
         try {
-          console.log("PRIVATE RPC REQUEST:", provider, req);
+          // The request is sent to the uHTTP router following the
+          // Ethereum JSON-RPC format.
           const response = await router.fetch(provider, {
-            body: JSON.stringify(req),
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              id: 1, // always use the same id
+              jsonrpc: "2.0",
+              method,
+              params,
+            }),
             method: "POST",
           });
-          console.log("PRIVATE RPC RESPONSE:", response);
 
           if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
           }
 
           const json = await response.json();
-          console.log("PRIVATE RPC RESPONSE:", json);
 
           return "error" in json ? json.error : json.result;
         } catch (e: any) {
